70dc7f7ad0d2ef1faf3f2cbf95bac1a43f6644bb
Jason Jōb <jason.allan.job@gmail.com>
2022-08-16 14:11:13 -0700

    added logging to install script

--- install.sh
+++ install.sh
@@ -63,13 +63,17 @@ stow nvim
 stow kitty
 
 # add zsh to valid login shells
+echo "Add zsh to valid login shells..."
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
+echo "Use zsh as default shell..."
 sudo chsh -s $(which zsh) $USER
 
 # bundle zsh plugins
+echo "Bundle zsh plugins..."
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh 
 
 # install neovim plugins
+echo "Install neovim plugins"
 nvim --headless +PlugInstall +qall
