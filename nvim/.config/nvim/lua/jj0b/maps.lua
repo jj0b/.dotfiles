1073c17e725c6ccf978dca9fd87e184f2c2b21be
Jason Jōb <jason.allan.job@gmail.com>
2022-09-14 10:14:57 -0700

    keymaps for moving text

--- nvim/.config/nvim/lua/jj0b/maps.lua
+++ nvim/.config/nvim/lua/jj0b/maps.lua
@@ -1,5 +1,7 @@
 local keymap = vim.keymap
 
+vim.g.mapleader = " "
+
 keymap.set('n', 'x', '"_x')
 
 -- Increment/decrement
@@ -17,9 +19,11 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sh', '<C-w>h')
@@ -32,3 +36,12 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- Move text up and down
+keymap.set('n', '<A-j>', ':m .+1<CR>==')
+keymap.set('n', '<A-k>', ':m .-2<CR>==')
+keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
+keymap.set('v', '<A-j>', [[:m '>+1<CR>gv=gv]])
+keymap.set('v', '<A-k>', [[:m '<-2<CR>gv=gv]])
+
