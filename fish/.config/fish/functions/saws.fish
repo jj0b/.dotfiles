2afd42915daaad2fbfb3cff028acb9c820c8d18a
Jason Jōb <jason.allan.job@gmail.com>
2022-12-05 13:33:54 -0800

    added aliases

---
+++ fish/.config/fish/functions/saws.fish
@@ -0,0 +1,4 @@
+function saws -d 'saws alias for authenticating with SiriusXM AWS'
+  java -jar ~/Code/SiriusXM/aws-cli/onelogin-aws-cli.jar --username jason.job@siriusxm.com --appid 1853592 --subdomain pandora --region us-east-1 --aws-account-id 790603163548 --aws-role-name SAML-PowerUser --profile sxm-platform-eng-cicd-prod
+  export AWS_PROFILE="sxm-platform-eng-cicd-prod"
+end
---
+++ fish/.config/fish/functions/sawsx.fish
@@ -0,0 +1,6 @@
+function sawsx -d 'sawsx alias for exporting code artifact vars after authenticating with saws alias'
+  export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token --domain enterprise --domain-owner 877812532776 --region us-east-1 --query authorizationToken --output text --profile sxm-platform-eng-cicd-prod )
+  export CODEARTIFACT_URL="https://enterprise-877812532776.d.codeartifact.us-east-1.amazonaws.com/npm/platform-eng-cicd" 
+  export CODEARTIFACT_REPO_NAME="platform-eng-cicd"
+  aws codeartifact login --tool npm --region us-east-1 --domain enterprise --domain-owner 877812532776 --repository platform-eng-cicd --profile sxm-platform-eng-cicd-prod
+end
