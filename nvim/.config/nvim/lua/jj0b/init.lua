79db84a10ad50dd2063ed0b01feb5aaea113b096
Jason Jōb <jason.allan.job@gmail.com>
2022-12-17 17:03:05 -0800

    cleaned up init.lua

--- nvim/.config/nvim/lua/jj0b/init.lua
+++ nvim/.config/nvim/lua/jj0b/init.lua
@@ -1,15 +1,3 @@
 require('jj0b.set')
 require('jj0b.remap')
 require('jj0b.packer')
-
-vim.cmd[[colorscheme nord]]
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-local is_mac = has "macunix"
-
-if is_mac then
-  vim.opt.clipboard:append { 'unnamedplus' }
-end
-
