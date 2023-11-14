afeb727286b8ed835d5dd82539ce05a175a11b73
Jason Jōb <jason.allan.job@gmail.com>
2023-11-14 12:06:53 -0800

    more golang setup

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,29 +1,43 @@
 local M = {}
 
 M.general = {
-  n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+	n = {
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "Window left" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "Window right" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "Window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "Window up" },
 
-    ["<C-A-j>"] = { ":m .+1<CR>==", "move line down" },
-    ["<C-A-k>"] = { ":m .-2<CR>==", "move line up" },
-  },
+		["<C-A-j>"] = { ":m .+1<CR>==", "Move line down" },
+		["<C-A-k>"] = { ":m .-2<CR>==", "Move line up" },
+	},
 
-  i = {
-    ["kj"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
+	i = {
+		["kj"] = { "<ESC>", "Escape insert mode", opts = { nowait = true } },
 
-    ["<C-A-j>"] = { "<ESC>:m .+1<CR>==gi", "move line down" },
-    ["<C-A-k>"] = { "<ESC>:m .-2<CR>==gi", "move line up" },
-  },
+		["<C-A-j>"] = { "<ESC>:m .+1<CR>==gi", "Move line down" },
+		["<C-A-k>"] = { "<ESC>:m .-2<CR>==gi", "Move line up" },
+	},
 
-  v = {
-    ["kj"] = { "<ESC>", "escape visual mode", opts = { nowait = true } },
+	v = {
+		["kj"] = { "<ESC>", "Escape visual mode", opts = { nowait = true } },
 
-    ["<C-A-j>"] = { [[:m '>+1<CR>gv==gv]], "move line down" },
-    ["<C-A-k>"] = { [[:m '<-2<CR>gv==gv]], "move line up" },
-  },
+		["<C-A-j>"] = { [[:m '>+1<CR>gv==gv]], "Move line down" },
+		["<C-A-k>"] = { [[:m '<-2<CR>gv==gv]], "Move line up" },
+	},
+}
+
+M.gopher = {
+	plugin = true,
+	n = {
+		["<leader>gsj"] = {
+			"<cmd> GoTagAdd json <CR>",
+			"Add json struct tags",
+		},
+		["<leader>gsy"] = {
+			"<cmd> GoTagAdd yaml <CR>",
+			"Add yaml struct tags",
+		},
+	},
 }
 
 return M
--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,74 +1,85 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
+	{
+		"neovim/nvim-lspconfig",
 
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require("custom.configs.null-ls")
-      end,
-    },
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
 
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "html-lsp",
-        "prettier",
-        "stylua",
-        "typescript-language-server",
-        "cssmodules-language-server",
-        "svelte-language-server",
-        "tailwindcss-language-server",
-        "emmet-ls",
-        "eslint_d",
-        "python-lsp-server",
-        "gopls",
-        "pyright",
-        "black",
-      },
-    },
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"prettier",
+				"stylua",
+				"typescript-language-server",
+				"cssmodules-language-server",
+				"svelte-language-server",
+				"tailwindcss-language-server",
+				"emmet-ls",
+				"eslint_d",
+				"python-lsp-server",
+				"gopls",
+				"pyright",
+				"black",
+			},
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
 
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "yaml",
-        "svelte",
-        "go",
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"yaml",
+				"svelte",
+				"go",
 
-        -- low level
-        "c",
-        "zig",
-      },
-    },
-  },
-  {
-    "windwp/nvim-ts-autotag",
-  },
-  {
-    "kylechui/nvim-surround",
-  },
+				-- low level
+				"c",
+				"zig",
+			},
+		},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+	},
+	{
+		"kylechui/nvim-surround",
+	},
+	{
+		"olexsmir/gopher.nvim",
+		ft = "go",
+		config = function(_, opts)
+			require("gopher").setup(opts)
+			require("core.utils").load_mappings("gopher")
+		end,
+		build = function()
+			vim.cmd([[silent! GoInstallDeps]])
+		end,
+	},
 }
 return plugins
