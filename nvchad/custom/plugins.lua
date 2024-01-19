0795b7f4875e414814f22d8b69662d1b6bb57aee
Jason Jōb <jason.allan.job@gmail.com>
2024-01-19 10:17:12 -0800

    updated TreeSitter default installs

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,92 +1,109 @@
 local plugins = {
-	{
-		"neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
 
-		dependencies = {
-			"jose-elias-alvarez/null-ls.nvim",
-			config = function()
-				require("custom.configs.null-ls")
-			end,
-		},
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require("custom.configs.null-ls")
+      end,
+    },
 
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"html-lsp",
-				"prettier",
-				"stylua",
-				"typescript-language-server",
-				"cssmodules-language-server",
-				"svelte-language-server",
-				"tailwindcss-language-server",
-				"emmet-ls",
-				"eslint_d",
-				"python-lsp-server",
-				"gopls",
-				"pyright",
-				"black",
-			},
-		},
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				-- defaults
-				"vim",
-				"lua",
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+        "typescript-language-server",
+        "cssmodules-language-server",
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "emmet-ls",
+        "eslint_d",
+        "python-lsp-server",
+        "gopls",
+        "pyright",
+        "black",
+      },
+    },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
 
-				-- web dev
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"tsx",
-				"json",
-				"yaml",
-				"svelte",
-				"go",
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "svelte",
+        "go",
+        "gomod",
+        "gosum",
+        "python",
 
-				-- low level
-				"c",
-				"zig",
-			},
-		},
-	},
-	{
-		"windwp/nvim-ts-autotag",
-	},
-	{
-		"kylechui/nvim-surround",
-	},
-	{
-		"airblade/vim-gitgutter",
-	},
-	{
-		"mg979/vim-visual-multi",
-		lazy = false,
-	},
-	{
-		"olexsmir/gopher.nvim",
-		ft = "go",
-		config = function(_, opts)
-			require("gopher").setup(opts)
-			require("core.utils").load_mappings("gopher")
-		end,
-		build = function()
-			vim.cmd([[silent! GoInstallDeps]])
-		end,
-	},
+        -- Ethereum
+        "solidity",
+
+        -- data formats
+        "yaml",
+        "toml",
+        "json",
+        "xml",
+
+        -- tools
+        "markdown",
+        "dockerfile",
+        "fish",
+        "make",
+        "gitignore",
+
+        -- low level
+        "c",
+        "zig",
+      },
+    },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+  },
+  {
+    "kylechui/nvim-surround",
+  },
+  {
+    "airblade/vim-gitgutter",
+  },
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd([[silent! GoInstallDeps]])
+    end,
+  },
 }
 return plugins
