75b2832caf76c4526de56ab96db899dcfab3c821
Jason Jōb <jason.allan.job@gmail.com>
2025-02-10 09:41:29 -0800

    made nvim transparent

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,17 +1,32 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "nord",
+  theme = "nord",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Normal = { bg = "NONE" },
+    NormalNC = { bg = "NONE" },
+    CursorLine = { bg = "NONE" },
+    CursorColumn = { bg = "NONE" },
+    SignColumn = { bg = "NONE" },
+    LineNr = { bg = "NONE" },
+    StatusLine = { bg = "NONE" },
+    StatusLineNC = { bg = "NONE" },
+    TelescopeNormal = { bg = "NONE" },
+    TelescopeBorder = { bg = "NONE" },
+    NvimTreeNormal = { bg = "NONE" },
+    NvimTreeNormalNC = { bg = "NONE" },
+    NvimTreeEndOfBuffer = { bg = "NONE" },
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
