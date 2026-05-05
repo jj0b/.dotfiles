bbc65ed6b7d36a96acd6625e9c2f5f30132943dd
Jason Jōb <jason.allan.job@gmail.com>
2026-05-05 15:30:15 -0700

    show hidden files in snacks.explorer

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = { hidden = true },
+          files = { hidden = true },
+          grep = { hidden = true },
+        },
+      },
+    },
+  },
+}
